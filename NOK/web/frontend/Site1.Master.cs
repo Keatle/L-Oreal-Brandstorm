1becaeeb6a44f4e39bad14ffec5faf94a06a39ec
Oreeditse Tlou <oreeditsetlou5@gmail.com>
2025-01-24 05:13:01 +0200

    frontend files added

---
+++ NOK/web/frontend/Site1.Master.cs
@@ -0,0 +1,33 @@
+﻿using AdonisMen.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdonisMen
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        Service1Client sr = new Service1Client();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            guest.Visible = true;
+            customer.Visible = false;
+            admin.Visible = false;
+            if (Session["LoggedInUser"] != null)
+            {
+                guest.Visible = false;
+                customer.Visible = true;
+                admin.Visible = false;
+            }
+            else if (Session["LoggedInAdmin"] != null)
+            {
+                guest.Visible = false;
+                customer.Visible = false;
+                admin.Visible = true;
+            }
+        }
+    }
+}
